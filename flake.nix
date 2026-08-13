fdf81f324af92df6507a6509d01779278ff6451b
Arjun Gore <arjun.c.gore@gmail.com>
2026-08-12 21:57:36 -0400

    mcsr-nixos (thx uku)

--- flake.nix
+++ flake.nix
@@ -13,20 +13,34 @@
             url = "github:youwen5/zen-browser-flake";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+
+        mcsr-nixos = {
+            url = "https://git.uku3lig.net/uku/mcsr-nixos/archive/main.tar.gz";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
     };
 
-    outputs = { self, nixpkgs, jay, zen-browser, ... }@inputs: {
+    outputs = { self, nixpkgs, jay, zen-browser, mcsr-nixos, ... }: {
         nixosConfigurations.snow = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
                 ./configuration.nix
                 jay.nixosModules.default
-                {
-                    programs.jay.enable = true;
-                    environment.systemPackages = [
-                        zen-browser.packages.x86_64-linux.default
-                    ];
-                }
+                mcsr-nixos.nixosModules.waywall
+                ({ pkgs, ... }:
+                    let
+                        mcsrPkgs = mcsr-nixos.packages.x86_64-linux;
+                    in {
+                        programs.jay.enable = true;
+                        environment.systemPackages = [
+                            zen-browser.packages.x86_64-linux.default
+                            mcsrPkgs.ninjabrain-bot
+                            (pkgs.prismlauncher.override {
+                                jdks = [ mcsrPkgs.graalvm-21 ];
+                            })
+                        ];
+                    })
             ];
         };
     };
