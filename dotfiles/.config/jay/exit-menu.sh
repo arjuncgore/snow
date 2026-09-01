b8d8c56fc8d4d022d50e557919df939917360e54
Arjun Gore <arjun.c.gore@gmail.com>
2026-08-31 20:42:47 -0400

    cleaned up bash scripts

--- dotfiles/.config/jay/exit-menu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-choice=$(echo -e "Exit\nCancel" | wofi --dmenu --width=100 --height=140 --prompt "Exit Jay?" --hide-scroll)
-
-if [ "$choice" = "Exit" ]; then
-    jay quit
-fi
-
--- dotfiles/.config/jay/shutdown-menu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-choice=$(echo -e "Shutdown\nCancel" | wofi --dmenu --width=100 --height=140 --prompt "Shutdown?" --hide-scroll)
-
-if [ "$choice" = "Shutdown" ]; then
-    shutdown now
-fi
-
