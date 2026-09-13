965bd0319db318a61627cdb9367f57fd8d68c44a
Arjun Gore <arjun.c.gore@gmail.com>
2026-09-13 19:04:07 -0400

    Squashed 'dotfiles/.config/waywall/' content from commit daad24a
    git-subtree-dir: dotfiles/.config/waywall
    git-subtree-split: daad24a05d1836e8a29e69e91e6f442ac5c1b309

---
+++ test.lua
@@ -0,0 +1,29 @@
+local waywall = require("waywall")
+local helpers = require("waywall.helpers")
+
+local M = {}
+
+M.send = function(config)
+    -- ========================== BEGIN TEST
+
+
+    -- local binds = {
+    --     decrement = "*-comma",
+    --     increment = "*-period",
+    --     undo = "*-bracketleft",
+    --     redo = "*-bracketright",
+    --     reset = "*-equal",
+    --     lock = "*-grave",
+    -- }
+    --
+    -- for action, bind in pairs(binds) do
+    --     config.actions[bind] = function()
+    --         waywall.exec("ninjabrain-bot-xwayland " .. action)
+    --     end
+    -- end
+
+
+    -- ========================== END TEST
+end
+
+return M
